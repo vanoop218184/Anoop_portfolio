8a9b882fa5386e35369712780181fb49180b3381
vanoop218184 <ankit5737v@gmail.com>
2024-07-26 09:49:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// header scrolling effect
+$(window).on("scroll", function () {
+  if ($(window).scrollTop()) {
+    $("header").addClass("nav-show");
+  } else {
+    $("header").removeClass("nav-show");
+  }
+});
+
+//dynamic footer content
+var year = new Date().getFullYear();
+var date = `©️ Anoop Verma ${year} . All Rights Reserved`;
+document.getElementsByTagName("footer")[0].innerHTML = date;
